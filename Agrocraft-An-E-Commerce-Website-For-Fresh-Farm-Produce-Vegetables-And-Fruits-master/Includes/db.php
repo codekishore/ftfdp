08d6100ef0d4e884bb18f0ee24c8c485a8ebc88b
Kishore <93311587+codekishore@users.noreply.github.com>
2024-06-06 19:58:07 +0530

    Delete Agrocraft-An-E-Commerce-Website-For-Fresh-Farm-Produce-Vegetables-And-Fruits-master directory

--- Agrocraft-An-E-Commerce-Website-For-Fresh-Farm-Produce-Vegetables-And-Fruits-master/Includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-        $con = mysqli_connect("localhost","root","","impulse101");
-      
-        if (mysqli_connect_errno()) {
-                echo "Failed to connect to MySql " . mysqli_connect_error();
-        }
